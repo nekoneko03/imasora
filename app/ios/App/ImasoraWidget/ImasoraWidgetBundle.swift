1990c021fa7c4fa9fbf4f1be6ccb72715b5d5bd8
nekoneko03 <ka228yu@gmail.com>
2026-07-08 21:10:09 +0900

    利用規約・プライバシーポリシー・運営者情報・お問い合わせページを追加

---
+++ app/ios/App/ImasoraWidget/ImasoraWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ImasoraWidgetBundle.swift
+//  ImasoraWidget
+//
+//  Created by 金子裕亮 on 2026/07/08.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ImasoraWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ImasoraWidget()
+        ImasoraWidgetControl()
+    }
+}
